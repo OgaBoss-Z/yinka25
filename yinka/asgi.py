d3961cf889e7574a4165865ee3a83f09767548cc
OgaBoss Z <tryogaboss@gmail.com>
2025-06-02 17:00:40 +0100

    Update asgi.py

--- yinka/asgi.py
+++ yinka/asgi.py
@@ -1,7 +1,9 @@
-
+from whitenoise import WhiteNoise
 import os
 
 from django.core.asgi import get_asgi_application
+
+application = WhiteNoise(application, root='media/', prefix='media/')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yinka.settings')
 
